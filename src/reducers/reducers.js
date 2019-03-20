bae1f46aa83ab62c41f2314798ddf0432e2f08ce
darc window <darc.windowwww@gmail.com>
2019-03-20 07:05:45 -0400

    request & receive reducers completed

---
+++ src/reducers/reducers.js
@@ -0,0 +1,19 @@
+import { GET_PHOTOS, RECEIVE_PHOTOS } from '../actions/actions'
+
+
+const initialState = {
+    photos: []
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case GET_PHOTOS:
+        return { ...state, loading: true }
+
+        case RECEIVE_PHOTOS:
+        let data = action.data
+        return { ...state, photos: data, loading: false }
+
+        default: return state
+    }
+}
