bc0417bf1d10c5bdffb65dae9d0befb56876333a
darc window <darc.windowwww@gmail.com>
2019-03-20 07:06:14 -0400

    added reducers to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import createSagaMiddleware from "redux-saga";
-// import reducers from '../reducers/reducers'
+import reducers from '../reducers/reducers'
 const saga = createSagaMiddleware()
 
 
 export const store = createStore(
+  reducers,
   applyMiddleware(saga)
 );
 
